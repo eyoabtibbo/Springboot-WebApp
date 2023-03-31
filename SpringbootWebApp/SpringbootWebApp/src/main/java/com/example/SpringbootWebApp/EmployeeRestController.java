61b1b8bf7945a0c41b12d280a571d032fc04227b
Matt Walsh <matt-walsh@users.noreply.github.com>
2023-03-31 13:22:31 -0300

    Add 404 response status when no employee found

--- SpringbootWebApp/SpringbootWebApp/src/main/java/com/example/SpringbootWebApp/EmployeeRestController.java
+++ SpringbootWebApp/SpringbootWebApp/src/main/java/com/example/SpringbootWebApp/EmployeeRestController.java
@@ -1,6 +1,7 @@
 package com.example.SpringbootWebApp;
 
 
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -44,28 +45,52 @@ public Employee getEmployeeId (int id) {
         }
         return null;
     };
+
+    public Employee getEmployeeByAddress (String address) {
+        for(int i=0; i < employees.length; i++) {
+            if ((employees[i].getAddress()).equals(address)) {
+                return employees[i];
+            }
+        }
+        return null;
+    };
     @GetMapping("/listEmployees")
     public Employee[] listEmployees() {
         return employees;
 
     }
     @GetMapping("/listEmployees/{id}")
-    public Employee listEmployeesId(@PathVariable int id) {
-        return getEmployeeId(id);
+    public Employee listEmployeesId(@PathVariable int id, HttpServletResponse response) {
+        Employee retrievedEmployee = getEmployeeId(id);
+        if(retrievedEmployee == null){
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        }
+        return retrievedEmployee;
     }
     @GetMapping("/listEmployees/firstname={firstName}")
-    public Employee listEmployeesFirstName(@PathVariable String firstName) {
-        return getEmployeeByFirstName(firstName);
+    public Employee listEmployeesFirstName(@PathVariable String firstName, HttpServletResponse response) {
+        Employee retrievedEmployee = getEmployeeByFirstName(firstName);
+        if(retrievedEmployee == null){
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        }
+        return retrievedEmployee;
     }
 
     @GetMapping("/listEmployees/lastname={lastName}")
-    public Employee listEmployeesLastName(@PathVariable String lastName) {
-        return getEmployeeByLastName(lastName);
+    public Employee listEmployeesLastName(@PathVariable String lastName, HttpServletResponse response) {
+        Employee retrievedEmployee = getEmployeeByLastName(lastName);
+        if(retrievedEmployee == null){
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        }
+        return retrievedEmployee;
     }
     @GetMapping("/listEmployees/address")
-    public Employee listEmployeesAddress(@PathVariable String address) {
-        Employee employee= employees[Integer.parseInt(address)];
-        return employee;
+    public Employee listEmployeesAddress(@PathVariable String address, HttpServletResponse response) {
+        Employee retrievedEmployee = getEmployeeByAddress(address);
+        if(retrievedEmployee == null){
+            response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+        }
+        return retrievedEmployee;
     }
 
 }
